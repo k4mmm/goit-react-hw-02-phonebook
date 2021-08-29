7271ee7c39a4c637ea664285f758c7fe29ec00b4
Kostia Chaikovskyi <chaichka175@gmail.com>
2021-08-29 22:57:27 +0300

    Updates

---
+++ static/js/main.067122e8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{10:function(t,e,n){},4:function(t,e){},5:function(t,e,n){"use strict";n.r(e);var o=n(0),c=n.n(o),i=n(3),r=n.n(i),s=(n(10),n(4)),a=n.n(s),h=n(2);r.a.render(Object(h.jsx)(c.a.StrictMode,{children:Object(h.jsx)(a.a,{})}),document.getElementById("root"))}},[[5,1,2]]]);
+//# sourceMappingURL=main.067122e8.chunk.js.map
